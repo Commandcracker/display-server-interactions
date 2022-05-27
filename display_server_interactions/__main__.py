90a443ac730b34b2031ac66569f29bb6aca9585f
Commandcracker <lukas.Fricke.dev@gmail.com>
2022-05-27 22:56:19 +0200

    fixed __main__.py

--- display_server_interactions/__main__.py
+++ display_server_interactions/__main__.py
@@ -5,14 +5,15 @@ from . import DSI
 
 
 def main() -> None:
-    window = DSI.get_active_window()
+    with DSI() as dsi:
+        window = dsi.get_active_window()
 
-    print("Active window: ")
-    print("\tName: {}".format(window.name))
-    print("\tPID: {}".format(window.pid))
-    if window.xid:
-        print("\tXID: {}".format(window.xid))
-    print("\tGeometry: {}".format(window.geometry))
+        print("Active window: ")
+        print("\tName: {}".format(window.name))
+        print("\tPID: {}".format(window.pid))
+        if window.xid:
+            print("\tXID: {}".format(window.xid))
+        print("\tGeometry: {}".format(window.geometry))
 
 
 if __name__ == "__main__":
