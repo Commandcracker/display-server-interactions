ab0e905c948d634bc74188d70c53398daf0381aa
Commandcracker <lukas.Fricke.dev@gmail.com>
2022-05-27 16:21:51 +0200

    fixed autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ from os.path import dirname, join, abspath
 import sphinx_rtd_theme
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+
+sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+)
 
 
 # -- Project information -----------------------------------------------------
